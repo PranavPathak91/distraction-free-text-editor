bf98d212aab782a1e6faf08e3789315163543611
Pranav <pranavpathak.91@gmail.com>
2024-11-24 15:48:46 +0100

    Refine editor layout and quiet mode styles

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useTextEditor } from '../../context/TextEditorContext';
 import MenuBar from '../MenuBar/MenuBar';
 import Editor from './Editor/Editor';
 import AIPanel from '../AIPanel/AIPanel';
 import './Layout.css';
 
 function Layout() {
+  const { state } = useTextEditor();
+  const isQuietMode = state.isQuietMode;
+
   return (
-    <div className="app-layout">
+    <div className={`app-layout ${isQuietMode ? 'quiet-mode' : ''}`}>
       <div className="menu-bar-container">
         <MenuBar />
       </div>
